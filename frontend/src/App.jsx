266ba5448a2dae7c1dcc1ac99e4705d4958a1560
gpt-anurag <anurag.dev1@protonmail.com>
2023-02-02 17:09:46 +0530

    styled upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
-import Header from "./components/Header";
+import Header from './components/Header'
+import styled from 'styled-components'
 
 export default function App() {
   return (
+    <main>
       <Header />
-  );
+    </main>
+  )
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,57 +1,67 @@
-import React, { useState } from "react";
-import axios from "axios";
-import JSONTable from "./Jsontable";
-import DataTable from "react-data-table-component";
+import React, { useState } from 'react'
+import axios from 'axios'
+import JSONTable from './Jsontable'
+import DataTable from 'react-data-table-component'
+import FormStyle from '../component-styles/FormStyle'
+import { GoCloudUpload } from 'react-icons/go'
+import Form from './Form'
+import Filters from './Filters'
 
 const Header = () => {
-  const [file, setFile] = useState(null);
-  const [table, setTable] = useState([{}]);
+  const [file, setFile] = useState(null)
+  const [table, setTable] = useState([{}])
+  const [isUploaded, setIsUploaded] = useState(false)
 
   const handleFileChange = (event) => {
-    const file_data = event.target.files[0];
-    setFile(file_data);
-  };
+    const file_data = event.target.files[0]
+    setFile(file_data)
+  }
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
-    const formData = new FormData();
-    formData.append("file", file);
+    event.preventDefault()
+    const formData = new FormData()
+    formData.append('file', file)
     try {
-      const response = await axios.post("http://127.0.0.1:5000/file-upload", formData);
-      console.log(response.data);
+      const response = await axios.post(
+        'http://127.0.0.1:5000/file-upload',
+        formData
+      )
       setTable(response.data)
+      setIsUploaded(true)
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  };
+  }
   const columns = React.useMemo(
     () =>
       Object.keys(table[0] || {}).map((key) => ({
-        name: key,
+        name: `Column ${key}`,
         selector: key,
         sortable: true,
       })),
     [table]
-  );
+  )
   return (
-    <>
-    <form onSubmit={handleSubmit}>
-    <input type="file" onChange={handleFileChange} />
-    <button type="submit">Upload</button>
-    </form>
-    <div style={{ overflow: "auto" }}>
-      {/* <JSONTable 
-      data = {table}/> */}
-      <DataTable
-      columns={columns}
-      data={table}
-      pagination
-      paginationPerPage={10}
-    />
-    </div>
-    </>
-    
-  );
-};
+    <FormStyle>
+      {!isUploaded && (
+        <Form handleSubmit={handleSubmit} handleFileChange={handleFileChange} />
+      )}
+      {isUploaded && (
+        <>
+          <Filters />
 
-export default Header;
+          <div style={{ overflow: 'auto' }} className='section-table'>
+            <DataTable
+              columns={columns}
+              data={table}
+              pagination
+              paginationPerPage={10}
+            />
+          </div>
+        </>
+      )}
+    </FormStyle>
+  )
+}
+
+export default Header
