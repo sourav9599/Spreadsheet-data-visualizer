171855dc2b56941be161267b7672e926af0d2b47
Sourav Mohanty <sourav10mohanty@gmail.com>
2023-02-16 15:52:48 +0530

    updated hompage

--- backend/app.py
+++ backend/app.py
@@ -8,5 +8,5 @@ app.config["DEBUG"] = True
 # Upload folder
 # UPLOAD_FOLDER = 'files'
 # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
 app.config['MAX_DF_COUNT'] = 100
