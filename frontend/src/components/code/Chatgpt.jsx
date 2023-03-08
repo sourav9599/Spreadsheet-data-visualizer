17cfd80383e0ca3f48e391f04fe6b7691d7d9334
gpt-anurag <anurag.dev1@protonmail.com>
2023-03-08 13:32:10 +0530

    code component modification

--- frontend/src/components/code/Chatgpt.jsx
+++ frontend/src/components/code/Chatgpt.jsx
@@ -1,147 +1,186 @@
-import { useRef, useState } from "react";
-import "./Chatgpt.css";
-import axios from "axios";
-import SyntaxHighlighter from "react-syntax-highlighter";
+import { useRef, useState } from 'react'
+import './Chatgpt.css'
+import axios from 'axios'
+import SyntaxHighlighter from 'react-syntax-highlighter'
 import {
-	docco,
-	atomOneLight,
-	atomOneDark,
-} from "react-syntax-highlighter/dist/esm/styles/hljs";
-import Suggestions from "./Suggestions";
+  docco,
+  atomOneLight,
+  atomOneDark,
+} from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import Suggestions from './Suggestions'
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+} from '@mui/material'
 
-const YOU = "ME";
-const AI = "AI";
+const YOU = 'ME'
+const AI = 'AI'
 function Chatgpt() {
-	const inputRef = useRef();
-	const [qna, setQna] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [hint, setHint] = useState(false);
-	const [hintData, setHintData] = useState();
+  const inputRef = useRef()
+  const [qna, setQna] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [hint, setHint] = useState(false)
+  const [hintData, setHintData] = useState()
 
-	const updateQNA = (from, value) => {
-		setQna((qna) => [...qna, { from, value }]);
-	};
+  const updateQNA = (from, value) => {
+    setQna((qna) => [...qna, { from, value }])
+  }
 
-	const handleSend = (ignoreHint) => {
-		const question = inputRef.current.value;
-		updateQNA(YOU, question);
-		setLoading(true);
-		setHint(false);
-		const params = {
-			ignore_hint: ignoreHint,
-		};
-		axios
-			.post(
-				"http://localhost:5000/chatgpt?" +
-					new URLSearchParams(params).toString(),
-				{
-					question,
-				}
-			)
-			.then((response) => {
-				if ("answer" in response.data) {
-					console.log(response.data.answer);
-					updateQNA(AI, response.data.answer);
-				}
-				if ("hint" in response.data) {
-					console.log(response.data);
-					setHintData(response.data);
-					setHint(true);
-				}
-			})
-			.catch((response) => {
-				console.log(response.data);
-				alert(response.data);
-			})
-			.finally(() => {
-				setLoading(false);
-			});
-	};
+  const handleSend = (ignoreHint) => {
+    const question = inputRef.current.value
+    updateQNA(YOU, question)
+    setLoading(true)
+    setHint(false)
+    const params = {
+      ignore_hint: ignoreHint,
+    }
+    axios
+      .post(
+        'http://localhost:5000/chatgpt?' +
+          new URLSearchParams(params).toString(),
+        {
+          question,
+        }
+      )
+      .then((response) => {
+        if ('answer' in response.data) {
+          console.log(response.data.answer)
+          updateQNA(AI, response.data.answer)
+        }
+        if ('hint' in response.data) {
+          console.log(response.data)
+          setHintData(response.data)
+          setHint(true)
+        }
+      })
+      .catch((response) => {
+        console.log(response.data)
+        alert(response.data)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
 
-	const renderContent = (qna) => {
-		const value = qna.value;
+  const renderContent = (qna) => {
+    const value = qna.value
 
-		if (Array.isArray(value)) {
-			return value.map((v) => (
-				<SyntaxHighlighter style={docco}>{v}</SyntaxHighlighter>
-			));
-		}
+    if (Array.isArray(value)) {
+      return value.map((v) => (
+        <SyntaxHighlighter style={docco}>{v}</SyntaxHighlighter>
+      ))
+    }
 
-		return <SyntaxHighlighter style={docco}>{value}</SyntaxHighlighter>;
-	};
-	return (
-		<main className="container">
-			<div className="chat-input">
-				<input
-					type="text"
-					ref={inputRef}
-					className="form-control col"
-					placeholder="Type Something"
-				/>
-				{hint ? (
-					<>
-						<button
-							disabled={loading}
-							className="btn btn-success"
-							onClick={() => handleSend(true)}
-						>
-							Re-Send
-						</button>
-						<Suggestions
-							setHint={setHint}
-							handleSend={handleSend}
-							hintData={hintData}
-						/>
-					</>
-				) : (
-					<button
-						disabled={loading}
-						className="btn btn-success"
-						onClick={() => handleSend(false)}
-					>
-						Send
-					</button>
-				)}
-			</div>
-			<div className="chats">
-				{qna.map((qna) => {
-					if (qna.from === YOU) {
-						return (
-							<div className="send chat">
-								<img
-									src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
-									alt=""
-									className="avtar"
-								/>
-								<p className="message-text">{qna.value}</p>
-							</div>
-						);
-					}
-					return (
-						<div className="recieve chat">
-							<img
-								src="https://cdn-icons-png.flaticon.com/512/9626/9626678.png"
-								alt=""
-								className="avtar"
-							/>
-							<p className="message-text">{renderContent(qna)}</p>
-						</div>
-					);
-				})}
+    return <SyntaxHighlighter style={docco}>{value}</SyntaxHighlighter>
+  }
+  return (
+    <main className='container'>
+      <div className='other-options'>
+        <Box sx={{ margin: '0 2rem', width: '15rem' }}>
+          <FormControl
+            fullWidth
+            sx={{ background: '#fafafa', marginBottom: '2rem' }}
+          >
+            <InputLabel id='demo-simple-select-label' variant='filled'>
+              Select Technology
+            </InputLabel>
+            <Select
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
+              value={100}
+              label='Age'
+              onChange={() => {}}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <Stack direction='column' spacing={2}>
+            <Button variant='contained'>Previously Searched</Button>
+            <Button variant='contained'>Time Limit</Button>
+            <Button variant='contained'>
+              Fine Tuned based on customer requirements
+            </Button>
+          </Stack>
+        </Box>
+      </div>
+      <div className='chat-input'>
+        <input
+          type='text'
+          ref={inputRef}
+          className='form-control col'
+          placeholder='Type Something'
+        />
+        {hint ? (
+          <>
+            <button
+              disabled={loading}
+              className='btn btn-success'
+              onClick={() => handleSend(true)}
+            >
+              Re-Send
+            </button>
+            <Suggestions
+              setHint={setHint}
+              handleSend={handleSend}
+              hintData={hintData}
+            />
+          </>
+        ) : (
+          <button
+            disabled={loading}
+            className='btn btn-success'
+            onClick={() => handleSend(false)}
+          >
+            Send
+          </button>
+        )}
+      </div>
+      <div className='chats'>
+        {qna.map((qna) => {
+          if (qna.from === YOU) {
+            return (
+              <div className='send chat'>
+                <img
+                  src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png'
+                  alt=''
+                  className='avtar'
+                />
+                <p className='message-text'>{qna.value}</p>
+              </div>
+            )
+          }
+          return (
+            <div className='recieve chat'>
+              <img
+                src='https://cdn-icons-png.flaticon.com/512/9626/9626678.png'
+                alt=''
+                className='avtar'
+              />
+              <p className='message-text'>{renderContent(qna)}</p>
+            </div>
+          )
+        })}
 
-				{loading && (
-					<div className="recieve chat">
-						<img
-							src="https://cdn-icons-png.flaticon.com/512/9626/9626678.png"
-							alt=""
-							className="avtar"
-						/>
-						<p>Typing...</p>
-					</div>
-				)}
-			</div>
-		</main>
-	);
+        {loading && (
+          <div className='recieve chat'>
+            <img
+              src='https://cdn-icons-png.flaticon.com/512/9626/9626678.png'
+              alt=''
+              className='avtar'
+            />
+            <p>Typing...</p>
+          </div>
+        )}
+      </div>
+    </main>
+  )
 }
 
-export default Chatgpt;
+export default Chatgpt
--- frontend/src/components/code/Home.jsx
+++ frontend/src/components/code/Home.jsx
@@ -1,56 +1,58 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
-import data from "./code-apps.json";
-import { Link } from "react-router-dom";
-
-console.log(data);
+import * as React from 'react'
+import Card from '@mui/material/Card'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { Button, CardActionArea } from '@mui/material'
+import data from './code-apps.json'
+import { Link } from 'react-router-dom'
+import { Stack } from '@mui/system'
 
 export default function ActionAreaCard() {
-	return (
-		<Box
-			sx={{
-				flexGrow: 1,
-				padding: 5,
-				alignContent: "center",
-			}}
-		>
-			<Grid
-				container
-				spacing={{ xs: 2, md: 3 }}
-				columns={{ xs: 4, sm: 8, md: 12 }}
-				direction="row"
-				justifyContent="space-evenly"
-				alignItems="center"
-			>
-				{data.map((tool) => (
-					<Grid xs={2} sm={4} md={4}>
-						<Card sx={{ maxWidth: 345 }}>
-							<CardActionArea component={Link} to="/code/chatgpt">
-								<CardMedia
-									component="img"
-									height="140"
-									image="../../assets/CODERFX.png"
-									alt={tool.name}
-								/>
-								<CardContent>
-									<Typography gutterBottom variant="h5" component="div">
-										{tool.name}
-									</Typography>
-									<Typography variant="body2" color="text.secondary">
-										{tool.description}
-									</Typography>
-								</CardContent>
-							</CardActionArea>
-						</Card>
-					</Grid>
-				))}
-			</Grid>
-		</Box>
-	);
+  return (
+    <Stack
+      sx={{
+        margin: '5em',
+      }}
+      spacing={5}
+    >
+      <Grid container columns={{ xs: 4, sm: 8, md: 12 }}>
+        {data.map((tool, idx) => {
+          return (
+            <Grid item key={idx} xs={2} sm={4} md={4}>
+              <Card
+                sx={{ maxWidth: '23rem', height: '100%', margin: '0 auto' }}
+              >
+                <CardActionArea component={Link} to='/code/chatgpt'>
+                  <CardMedia
+                    component='img'
+                    height='140'
+                    image={tool.image_path}
+                    alt={tool.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant='h5' component='div'>
+                      {tool.name}
+                    </Typography>
+                    <Typography variant='body2' color='text.secondary'>
+                      {tool.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          )
+        })}
+      </Grid>
+
+      <Button
+        variant='contained'
+        sx={{ width: 'fit-content', position: 'relative', left: '50px' }}
+      >
+        Explore Other Options
+      </Button>
+    </Stack>
+  )
 }
